b0bfd9c3733ad66b94f9981fd7cd9387b804f8a7
jukrapopk <jukrapopk@gmail.com>
2024-05-06 13:20:04 +0700

    Install mantine + basic page setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+import { MantineProvider, createTheme } from '@mantine/core'
+import '@mantine/core/styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const theme = createTheme({})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
