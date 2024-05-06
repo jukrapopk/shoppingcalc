72a73d6032f33e84a83616ca0ab126e1ae2d3a9e
jukrapopk <jukrapopk@gmail.com>
2024-05-06 23:16:07 +0700

    Implement inverted mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { Button, Card, Center, Group, NumberInput, Stack, Table, Text } from '@mantine/core';
+import { ActionIcon, Button, Card, Center, Group, NumberInput, Stack, Table, Text } from '@mantine/core';
+import { IconArrowsDoubleSwNe } from '@tabler/icons-react';
 import { useMemo, useState } from 'react';
 import { getMaxIndex, getMinIndex } from './helper';
 
 interface Item {
   price: number | undefined,
   count: number | undefined,
-  per: number | undefined,
-  // priceLabel: 'THB',
-  // countLabel: 'Unit'
+  per: number | undefined
 }
 
 const defaultItem: Item = { price: undefined, count: undefined, per: undefined }
@@ -59,23 +58,25 @@ function App() {
     }))
   }
 
+  const [inverted, setInverted] = useState(false);
+
   const rows = useMemo(() => {
     return items.map((item, i) => (
       <Table.Tr key={i} bg={shouldShowIndicator ? (minIndex === i ? "green.3" : maxIndex === i ? "red.4" : "none") : "none"}>
         <Table.Td>
-          <NumberInput inputMode="decimal" variant="filled" value={item.price ?? ''} onChange={(value) => onChange(i, 'price', value ? Number(value) : undefined)} size="md" />
+          <NumberInput inputMode="decimal" variant="filled" value={item.price ?? ''} onChange={(value) => onChange(i, 'price', value ? Number(value) : undefined)} size="md" hideControls />
         </Table.Td>
         <Table.Td>
-          <NumberInput inputMode="decimal" variant="filled" value={item.count ?? ''} onChange={(value) => onChange(i, 'count', value ? Number(value) : undefined)} size="md" />
+          <NumberInput inputMode="decimal" variant="filled" value={item.count ?? ''} onChange={(value) => onChange(i, 'count', value ? Number(value) : undefined)} size="md" hideControls />
         </Table.Td>
         <Table.Td>
           <Text>
-            {item.per ? Number(item.per).toFixed(2) : '-'}
+            {item.per ? Number(inverted ? 1 / item.per : item.per).toFixed(2) : '-'}
           </Text>
         </Table.Td>
       </Table.Tr>
     ))
-  }, [items, minIndex, maxIndex, shouldShowIndicator])
+  }, [items, minIndex, maxIndex, shouldShowIndicator, inverted])
 
   return (
     <Center p="md">
@@ -89,7 +90,22 @@ function App() {
               <Table.Tr>
                 <Table.Th>Price</Table.Th>
                 <Table.Th>Amount</Table.Th>
-                <Table.Th miw="6em">Price / Unit</Table.Th>
+                <Table.Th>
+                  <Group justify="space-between" wrap="nowrap">
+                    <Text size="sm" fw="bold">
+                      {inverted ?
+                        "Unit / Price"
+                        :
+                        "Price / Unit"
+                      }
+                    </Text>
+                    <Group gap={2}>
+                      <ActionIcon variant="transparent" size="xs" color="gray" onClick={() => setInverted(inverted => !inverted)}>
+                        <IconArrowsDoubleSwNe />
+                      </ActionIcon>
+                    </Group>
+                  </Group>
+                </Table.Th>
               </Table.Tr>
             </Table.Thead>
             <Table.Tbody>
